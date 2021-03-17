526761a41a049c6bc1b6a6f594c605c960d966f7
Gladys Noh <gladys.noh@icloud.com>
2021-03-16 22:30:33 -0600

    Added role on checkout test

--- page_model/tests/CheckoutTest.js
+++ page_model/tests/CheckoutTest.js
@@ -5,18 +5,15 @@ import checkOutPage from '../pages/CheckoutPage';
 import overViewPage from '../pages/OverviewPage';
 
 import {CREDENTIALS,CHECKOUTINFORMATION} from '../data/Constants';
+import { standardUser } from '../data/Roles';
 
 fixture('Checkout tests')
-.page `https://www.saucedemo.com/`
 .beforeEach(async(t)=>{
-    await loginPage.doLogin(CREDENTIALS.STANDARD_USERNAME, CREDENTIALS.PASSWORD);
+    await t.useRole(standardUser);
     // For all checkout test we are gonna add a single item and then go to the shopping cart
     await productListPage.addSingleItemAndGoToCart();
     // Also we need to click the checkout utton located under the shopping cart page.
     await t.click(shoppingCartPage.checkOutBtn);
-})
-.afterEach(async()=>{
-    await loginPage.doLogout();
 });
 
 test('7.- Click continue button without First Name information', async(t) =>{
